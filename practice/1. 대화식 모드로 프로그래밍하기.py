35152b506be012dd92ea7e9489b40fbc1385eb48
antarachain <hoonbitcoin@gmail.com>
2020-07-21 23:35:47 +0900

    2020_07_20

---
+++ practice/1. 대화식 모드로 프로그래밍하기.py
@@ -0,0 +1,24 @@
+print('안녕하세요')
+
+print('안녕하세요')
+a = 1
+b = 1
+print(a+b)
+
+_myname = 'samsjang'
+my_name = '홍길동'
+MyName2 = 'Hong gil-dong'
+counter = 1
+Counter = 2
+
+print(_myname)
+print(my_name)
+print(MyName2)
+print(counter)
+print(Counter)
+
+number1 = 1
+pi = 3.14
+flag = True
+char = 'x'
+chars 'I love Python'
